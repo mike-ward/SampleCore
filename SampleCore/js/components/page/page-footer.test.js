a6aa3fc80c8820fe216e9588f502e5abb87c26a8
mike-ward <mike@mike-ward.net>
2018-03-02 11:17:45 -0500

    add more tests

---
+++ SampleCore/js/components/page/page-footer.test.js
@@ -0,0 +1,6 @@
+﻿require('../../app.setup.tests');
+
+test('page-footer should contain footer class', () => {
+  var out = mq(App.Components.pageFooter);
+  out.should.have('.footer');
+})
---
+++ SampleCore/js/components/page/page-header.test.js
@@ -0,0 +1,17 @@
+﻿require('../../app.setup.tests');
+
+test('page-hearder should contain menu', () => {
+  const navbarOptions = {
+    items: [
+      { name: 'Home', link: 'home' },
+      { name: 'Item1', link: 'item1' },
+      { name: 'Item2', link: 'item2' },
+      { name: 'About', link: 'about' }
+    ]
+  }
+
+  var out = mq(App.Components.pageHeader, { options: navbarOptions });
+  out.should.have('.header');
+  out.should.have('.nav-bar');
+  out.should.have(4, 'a');
+})
