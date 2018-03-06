9cd696fea761bdf13f0acfb8a3d6f6574ad03780
mike-ward <mike@mike-ward.net>
2018-03-05 20:31:48 -0500

    remove webpack stuff

--- SampleCore/src/js/webpack-prod.js
+++
@@ -1,9 +0,0 @@
-﻿const merge = require('webpack-merge');
-const Uglify = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  plugins: [new Uglify({ sourceMap: true})]
-});
