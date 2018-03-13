5df2cfe8b54bf2230a3a17e5713e98399fa524c5
mike-ward <mike@mike-ward.net>
2018-03-13 10:00:32 -0400

    add some middle ware

--- SampleCore/Startup.cs
+++ SampleCore/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SampleCore.Infrastructure;
 using SampleCore.Models.Account.FileIdentityStore;
 using SampleCore.Models.Account.User;
@@ -20,18 +22,31 @@ namespace SampleCore
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddElm();
             services.AddResponseCompression();
+            ConfigureSessionState(services);
             ConfigureAuthenticationServices(services);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             ConfigureEnvironment(app, env);
+            ConfigureLogging(app, loggerFactory);
             app.UseResponseCompression();
+            app.UseSession();
             app.UseAuthentication();
             app.UseStaticFiles();
             app.UseMvc();
+        }
 
+        private static void ConfigureSessionState(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(15);
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         private static void ConfigureAuthenticationServices(IServiceCollection services)
@@ -66,6 +81,15 @@ namespace SampleCore
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
+        }
+
+        private void ConfigureLogging(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseElmPage();
+            app.UseElmCapture();
+            app.UseExceptionHandler();
         }
     }
 }
