aab9feb669f4270c3109de4fa3a88d76d7695f94
mike-ward <mike@mike-ward.net>
2018-03-29 15:56:51 -0400

    add home page content to test grid

--- SampleCore/Controllers/Api/SampleApiController.cs
+++ SampleCore/Controllers/Api/SampleApiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace SampleCore.Controllers.Api
 {
@@ -6,6 +8,17 @@ namespace SampleCore.Controllers.Api
     public class SampleApiController : Controller
     {
         [HttpGet("version")]
-        public string Version() => "0.0.1";
+        public string Version()
+        {
+            return "0.0.1";
+        }
+
+        [HttpGet("markets")]
+        public object Markets()
+        {
+            var client = new System.Net.WebClient();
+            var markets = client.DownloadString(new Uri("https://api.iextrading.com/1.0/stock/market/list/mostactive"));
+            return JsonConvert.DeserializeObject(markets);
+        }
     }
 }
--- SampleCore/app/js/pages/home.ts
+++ SampleCore/app/js/pages/home.ts
@@ -1,11 +1,46 @@
 ﻿import m from 'mithril';
+import { grid } from '../components/widgets/grid';
+import { IGridOptions, IGridColumn } from '../components/widgets/grid-options';
+import { camelIdentifierToTitle, dateToLocaleString } from '../services/convert-service';
+import { loadStyles } from '../services/dom-service';
+
+let gridOptions = { columns: [] as any[], data: [] as any[] } as IGridOptions;
 
 function view() {
   return m('div', [
-    m('h2', 'Welcome')
+    m('h2', 'Welcome'),
+    m(grid, { 'class': 'home-markets-grid', gridOptions: gridOptions })
   ]);
 }
 
+function oncreate() {
+  getMarkets()
+    .then(r => {gridOptions = loadGrid(r)})
+    .catch(r => r);
+}
+
+function getMarkets() {
+  return m.request({ url: 'api/sampleapi/markets', data: { r: Date.now() } });
+}
+
+function loadGrid(data: any) {
+  const fields = ['symbol', 'companyName', 'primaryExchange', 'sector', 'latestPrice', 'open', 'close', 'high', 'low', 'week52High', 'week52Low'];
+
+  const columns: IGridColumn[] = fields
+    .map(field => ({
+      id: field,
+      title: camelIdentifierToTitle(field),
+      allowSort: true,
+    }));
+
+  return { columns: columns, data: data };
+}
+
+// language=css
+const css = `.home-markets-grid { font-size: smaller; }`;
+loadStyles(css);
+
 export var home = {
-  view: view
+  view: view,
+  oncreate: oncreate
 }
