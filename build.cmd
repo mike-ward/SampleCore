7384ea4980040f5dfa51e963ba4b9895fc7d9721
mike-ward <mike@mike-ward.net>
2018-03-31 11:08:40 -0400

    allow solution to manage nuget package restore

--- build.cmd
+++ build.cmd
@@ -2,32 +2,12 @@
 
 echo.
 echo ##########################################
-echo ###      Restore Nuget Packages        ###
-echo ##########################################
-echo.
-
-nuget restore SampleCore.sln
-if ERRORLEVEL 1 goto END
-
-echo.
-echo ##########################################
 echo ###        Install NPM Packages        ###
 echo ##########################################
 echo.
 
 pushd SampleCore\app\js
 call npm install
-popd
-if ERRORLEVEL 1 goto END
-
-echo.
-echo ##########################################
-echo ###      Build JavaScript Assets       ###
-echo ##########################################
-echo.
-
-pushd SampleCore\app\js
-call npm run build
 popd
 if ERRORLEVEL 1 goto END
 
